5ed9135227310c929fa950d56769999f7d21d08b
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-02-26 04:36:05 +0300

    v0.4.1 - bugfix imports

--- botspot/components/data/user_data.py
+++ botspot/components/data/user_data.py
@@ -327,8 +327,8 @@ def setup_dispatcher(dp: Dispatcher, **kwargs):
         dp.message.middleware(UserTrackingMiddleware(cache_ttl=settings.cache_ttl))
 
     if settings.user_types_enabled:
-        from botspot.components import bot_commands_menu
-        from botspot.components.settings.bot_commands_menu import Visibility
+        from botspot import commands_menu as bot_commands_menu
+        from botspot.commands_menu import Visibility
 
         router = Router(name="user_data")
         router.message.filter(AdminFilter())  # Only admins can use these commands
