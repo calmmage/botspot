d69126e15f50f35f22ed9ee35b2227c29a6ce398
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-03-30 01:22:06 +0300

    v0.7.5 - add easter eggs!

--- botspot/utils/easter_eggs/main.py
+++ botspot/utils/easter_eggs/main.py
@@ -1,2 +1,14 @@
+import yaml
+import random
+from pathlib import Path
+
+file_path = Path(__file__).parent / "easter_eggs.yaml"
+easter_eggs = yaml.safe_load(open(file_path))["items"]
+
+
 def get_easter_egg() -> str:
-    return "Nah, I got nothing. Sorry."
+    return random.choice(easter_eggs)
+
+
+if __name__ == "__main__":
+    print(get_easter_egg())
