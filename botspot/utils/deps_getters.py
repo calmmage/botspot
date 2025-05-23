3ce588178688d25d8ad22c30470c5096c12378e6
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-05-23 21:50:16 +0300

    v0.10.17 - update get_telethon_client to use single_user by default if enabled

--- botspot/utils/deps_getters.py
+++ botspot/utils/deps_getters.py
@@ -11,12 +11,15 @@ from typing import TYPE_CHECKING, Optional
 from botspot.components.data.mongo_database import get_database, get_mongo_client
 from botspot.components.data.user_data import get_user_manager
 from botspot.components.main.event_scheduler import get_scheduler
+from botspot.components.middlewares.simple_user_cache import get_simple_user_cache
+from botspot.components.main.single_user_mode import get_single_user, is_single_user_mode_enabled
 from botspot.components.main.telethon_manager import get_telethon_manager
 from botspot.components.new.chat_binder import get_chat_binder
 from botspot.components.new.chat_fetcher import get_chat_fetcher
 from botspot.components.new.llm_provider import get_llm_provider
 from botspot.components.new.queue_manager import get_queue_manager
 from botspot.components.new.s3_storage import S3StorageProvider
+from botspot.utils.user_ops import to_user_record
 
 if TYPE_CHECKING:
     from aiogram import Bot, Dispatcher
@@ -49,13 +52,22 @@ def get_dispatcher() -> "Dispatcher":
 
 # Define get_telethon_client here to avoid circular imports
 async def get_telethon_client(
-    user_id: int, state: Optional["FSMContext"] = None
+    user_id: Optional[int] = None, state: Optional["FSMContext"] = None
 ) -> "TelegramClient":
     """
     Get a TelegramClient instance for a specific user.
     If state is provided and auto_auth is enabled, will trigger authentication if client is missing.
     """
     telethon_manager = get_telethon_manager()
+    if user_id is None:
+        # assume single user mode
+        if not is_single_user_mode_enabled():
+            raise ValueError("user_id is required in single user mode")
+        user = get_single_user()
+        assert user is not None
+        # todo: verify this flow works correctly
+        user_id = to_user_record(user).user_id
+        assert user_id is not None
     client = await telethon_manager.get_client(user_id, state)
     if not client:
         from botspot.core.errors import TelethonClientNotConnectedError
@@ -88,4 +100,5 @@ __all__ = [
     "get_queue_manager",
     "get_llm_provider",
     "get_s3_storage",
+    "get_simple_user_cache",
 ]
